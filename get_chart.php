2334d45f1d087021ddee05575ee1c6efd07c4814
Jake <jakewellsd@gmail.com>
2022-03-29 21:38:30 +0100

    add basic returning of an image

--- get_chart.php
+++ get_chart.php
@@ -1,15 +1,21 @@
 <?php
+
 require_once('vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
 $lastfm_key = $_ENV['LASTFM_KEY'];
 $username=$_POST['username'];
-$json = file_get_contents('https://ws.audioscrobbler.com/2.0/?'.
-                            'method=user.getweeklyalbumchart&'.
+$json = file_get_contents('http://ws.audioscrobbler.com/2.0/?'.
+                            'method=user.gettopalbums&'.
                             'user='.$username.'&'.
                             'api_key='.$lastfm_key.'&'.
                             'format=json');
 $obj = json_decode($json);
-echo $obj->weeklyalbumchart->album[0]->name;
-
+$url = $obj->topalbums->album[0]->image[3]->{'#text'};
+$image = file_get_contents($url);
+$img = new Imagick();
+$img -> readImageBlob($image);
+header('Content-Type: image/png');
+echo $img;
 ?>
