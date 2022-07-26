ecba8580469f6fda26a55fb0400873c0ea0adfdd
Jake <jakewellsd@gmail.com>
2022-07-26 21:11:11 +0100

    replace magic numbers with IMAGE_HEIGHT and IMAGE_WIDTH

--- classes/collageChart.php
+++ classes/collageChart.php
@@ -5,6 +5,8 @@
     
         const HEIGHT_BETWEEN_TEXT = 50.0;
         const TEXT_STARTING_HEIGHT = 44.0;
+        const IMAGE_HEIGHT = 300;
+        const IMAGE_WIDTH = 300;
         private $drawSettings;
         private $albums;
         public $gridSize;
@@ -26,7 +28,7 @@
         public function generateChart(){
             $chart = new Imagick();
             // Create canvas to place images upon
-            $chart->newImage(300 * $this->gridSize,300 * $this->gridSize, new ImagickPixel('black'));
+            $chart->newImage(self::IMAGE_HEIGHT * $this->gridSize, self::IMAGE_WIDTH * $this->gridSize, new ImagickPixel('black'));
             $chart->setFormat("png");
 
             $i = 0;
@@ -50,8 +52,8 @@
                 // half the image size
                 $img->resizeImage($img->getimageWidth()/2,$img->getimageHeight()/2,0,0);               
                 // Work out the next position on the grid to place the image
-                $xPosition = ($i % $this->gridSize) * 300;
-                $yPosition = (intdiv($i, $this->gridSize)) * 300;
+                $xPosition = ($i % $this->gridSize) * self::IMAGE_WIDTH;
+                $yPosition = (intdiv($i, $this->gridSize)) * self::IMAGE_HEIGHT;
                 // place the image on the canvas
                 $chart->compositeImage($img, Imagick::COMPOSITE_DEFAULT, $xPosition, $yPosition);
                 $i++;
