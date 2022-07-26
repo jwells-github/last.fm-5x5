fa082c1ac735e37ad0d8b2613d7df45ded3a484b
Jake <jakewellsd@gmail.com>
2022-07-26 21:23:58 +0100

    add tests for collageChart.php

---
+++ tests/collageChartTest.php
@@ -0,0 +1,49 @@
+<?php
+
+    use PHPUnit\Framework\TestCase;
+    require_once  './classes/request.php';
+    require_once  './classes/collageChart.php';
+    require_once  './classes/album.php';
+
+    final class collageChartTest extends TestCase{
+
+        public function testCollageChartGridSize(){
+            $jsonData = json_decode(file_get_contents("tests/testData.json"));
+            $albumArray = array();
+            for($i=0; $i<4; $i++){
+                $albumArray[] = new Album($jsonData->topalbums->album[$i]);
+            }
+
+            $collageChart = new CollageChart($albumArray);
+
+            $this->assertSame(count($albumArray),4);
+            $this->assertSame($collageChart->gridSize,2.0);
+        }
+
+        public function testCollageChartGridSizeRoundsUp(){
+            $jsonData = json_decode(file_get_contents("tests/testData.json"));
+            $albumArray = array();
+            for($i=0; $i<5; $i++){
+                $albumArray[] = new Album($jsonData->topalbums->album[$i]);
+            }
+
+            $collageChart = new CollageChart($albumArray);
+
+            $this->assertSame(count($albumArray),5);
+            $this->assertSame($collageChart->gridSize,3.0);
+        }
+
+        public function testCollageChartReturnsPng(){
+            $jsonData = json_decode(file_get_contents("tests/testData.json"));
+            $albumArray = array();
+            for($i=0; $i<1; $i++){
+                $albumArray[] = new Album($jsonData->topalbums->album[$i]);
+            }
+
+            $collageChart = new CollageChart($albumArray);
+
+            $this->assertSame($collageChart->generateChart()->getFormat(),"png");
+        }   
+    }
+
+?>
